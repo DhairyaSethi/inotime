d4f8b19f6616e4464ecc6d5dca38f10d19723d94
Dhairya Sethi <dhairya.rkp@gmail.com>
2020-06-28 04:58:46 +0530

    deployed

--- truffle-config.js
+++ truffle-config.js
@@ -61,12 +61,9 @@ module.exports = {
     // NB: It's important to wrap the provider as a function.
     rinkeby: {
       provider: () => new HDWalletProvider(mnemonic, `https://rinkeby.infura.io/v3/e0545923a679490dbad2b378f01621ac`),
-      network_id: ,       // Ropsten's id
+      network_id: 4,       // Ropsten's id
       gas: 4500000,
       gasPrice: 10000000000,       // Ropsten has a lower block limit than mainnet
-      confirmations: 2,    // # of confs to wait between deployments. (default: 0)
-      timeoutBlocks: 200,  // # of blocks before a deployment times out  (minimum/default: 50)
-      skipDryRun: true     // Skip dry run before migrations? (default: false for public nets )
     },
 
     // Useful for private networks
